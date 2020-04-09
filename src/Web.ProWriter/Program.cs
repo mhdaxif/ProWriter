60353ecdb7ccb445d56b03bf49bb8d43eaeb3292
Asif <mhdaxif@gmail.com>
2020-04-09 15:20:47 +0500

    tiny fixes

--- src/Web.ProWriter/Program.cs
+++ src/Web.ProWriter/Program.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Web.ProWriter
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var context = scope.ServiceProvider.GetService<AppDbContext>();
+                context.Database.Migrate();
+
+                SeedDb.Seed(serviceProvider);
+            }
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args);
+            host.ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+
+            return host;
+        }
     }
 }
---
+++ src/Web.ProWriter/SeedDb.cs
@@ -0,0 +1,29 @@
+﻿using Data.ProWriter;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.ProWriter
+{
+    static public class SeedDb
+    {
+        static public void Seed(IServiceProvider serviceProvider)
+        {
+            using (var context = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>()))
+            {
+                // Seed users
+                if (context.Users.Any() == false)
+                {
+                    context.Users.AddRange(SeedData.GetUsers());
+                }
+
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
